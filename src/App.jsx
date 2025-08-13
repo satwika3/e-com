760a11773f44270144e1311a06168f9bb3a81618
satwika3 <2300030465@kluniversity.in>
2025-08-13 09:49:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S219_CICD">
+    <Router basename="/e-com">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
